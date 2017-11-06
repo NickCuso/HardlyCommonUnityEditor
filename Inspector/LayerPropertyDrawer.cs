a42e55a04161b87970a4c7ec9fb239ebc0155192
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-11-06 11:59:34 -0800

    V1

---
+++ Inspector/LayerPropertyDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace HD
+{
+  [CustomPropertyDrawer(typeof(Layer))]
+  public class LayerPropertyDrawer : PropertyDrawer
+  {
+    public override void OnGUI(
+      Rect position, 
+      SerializedProperty property, 
+      GUIContent label)
+    {
+      EditorGUI.BeginProperty(position, GUIContent.none, property);
+      SerializedProperty layerIndex = property.FindPropertyRelative("layerIndex");
+      position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+      if (layerIndex != null)
+      {
+        layerIndex.intValue = EditorGUI.LayerField(position, layerIndex.intValue);
+      }
+      EditorGUI.EndProperty();
+    }
+  }
+}
